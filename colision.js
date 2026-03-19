28964cedde01cb65639b369e1b709a1815a1a045
sebasislas73 <josebmont05@gmail.com>
2026-03-18 22:46:18 -0600

    Agrego practica 2.5 interactividad

--- colision.js
+++ colision.js
@@ -1,143 +1,134 @@
 const canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
-canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-class Circle {
+let estrellas = [];
+let score = 0;
 
-constructor(x,y,radius,color,text,speed){
+let img = new Image();
+img.src = "estrella.png";
 
-this.posX = x;
-this.posY = y;
-this.radius = radius;
-this.color = color;
-this.originalColor = color;
-this.text = text;
+class Estrella{
+
+constructor(x,y,size,speed){
+
+this.x = x;
+this.y = y;
+this.size = size;
 this.speed = speed;
 
-this.dx = (Math.random()>0.5?1:-1)*speed;
-this.dy = (Math.random()>0.5?1:-1)*speed;
+this.rotation = Math.random()*360;
+this.rotationSpeed = Math.random()*0.1;
 
 }
 
-draw(context){
+draw(){
 
-context.beginPath();
+ctx.save();
 
-context.strokeStyle = this.color;
-context.lineWidth = 2;
+ctx.translate(this.x+this.size/2,this.y+this.size/2);
 
-context.textAlign = "center";
-context.textBaseline = "middle";
-context.font = "16px Arial";
+ctx.rotate(this.rotation);
 
-context.fillText(this.text,this.posX,this.posY);
+ctx.shadowColor="yellow";
+ctx.shadowBlur=30;
 
-context.arc(this.posX,this.posY,this.radius,0,Math.PI*2,false);
-context.stroke();
+ctx.drawImage(img,-this.size/2,-this.size/2,this.size,this.size);
 
-context.closePath();
+ctx.restore();
 
 }
 
-update(context){
+update(){
 
-this.draw(context);
+this.y += this.speed;
+this.rotation += this.rotationSpeed;
 
-this.posX += this.dx;
-this.posY += this.dy;
+if(this.y > canvas.height){
 
-if(this.posX + this.radius > canvas.width || this.posX - this.radius < 0){
-this.dx = -this.dx;
-}
-
-if(this.posY + this.radius > canvas.height || this.posY - this.radius < 0){
-this.dy = -this.dy;
-}
+this.reset();
 
 }
 
+this.draw();
+
 }
 
-let circles = [];
+reset(){
 
-function generateCircles(n){
+this.y = -100;
+this.x = Math.random()*canvas.width;
+
+}
+
+}
+
+function generarEstrellas(n){
 
 for(let i=0;i<n;i++){
 
-let radius = Math.random()*30+20;
+let size = Math.random()*90+70;
 
-let x = Math.random()*(canvas.width-radius*2)+radius;
-let y = Math.random()*(canvas.height-radius*2)+radius;
+let x = Math.random()*canvas.width;
+let y = Math.random()*-800;
 
-let color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+let speed = Math.random()*2+2;
 
-let speed = Math.random()*4+1;
-
-let text = "C"+(i+1);
-
-circles.push(new Circle(x,y,radius,color,text,speed));
+estrellas.push(new Estrella(x,y,size,speed));
 
 }
 
 }
 
-function detectCollisions(){
+canvas.addEventListener("click",(event)=>{
 
-for(let i=0;i<circles.length;i++){
+let mouseX = event.clientX;
+let mouseY = event.clientY;
 
-for(let j=i+1;j<circles.length;j++){
+estrellas.forEach(e=>{
 
-let c1 = circles[i];
-let c2 = circles[j];
+if(
 
-let dx = c2.posX - c1.posX;
-let dy = c2.posY - c1.posY;
+mouseX > e.x &&
+mouseX < e.x + e.size &&
+mouseY > e.y &&
+mouseY < e.y + e.size
 
-let distance = Math.sqrt(dx*dx + dy*dy);
+){
 
-let minDistance = c1.radius + c2.radius;
+score++;
 
-if(distance < minDistance){
+document.getElementById("contador").innerText = score;
 
-c1.color = "#0000FF";
-c2.color = "#0000FF";
+e.reset();
 
-/* calcular cuánto se están encimando */
-let overlap = minDistance - distance;
+}
 
-/* normalizar dirección */
-let nx = dx / distance;
-let ny = dy / distance;
+});
 
-/* separarlos */
-c1.posX -= nx * overlap/2;
-c1.posY -= ny * overlap/2;
+});
 
-c2.posX += nx * overlap/2;
-c2.posY += ny * overlap/2;
+function dificultad(){
 
-/* invertir velocidades */
-let tempDx = c1.dx;
-let tempDy = c1.dy;
+estrellas.forEach(e=>{
 
-c1.dx = c2.dx;
-c1.dy = c2.dy;
+if(score>20){
 
-c2.dx = tempDx;
-c2.dy = tempDy;
+e.speed=10;
+
+}else if(score>10){
+
+e.speed=6;
 
 }else{
 
-c1.color = c1.originalColor;
-c2.color = c2.originalColor;
+e.speed=3;
 
 }
 
-}
-
-}
+});
 
 }
 
@@ -145,16 +136,16 @@ function animate(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
-circles.forEach(circle=>{
-circle.update(ctx);
+estrellas.forEach(e=>{
+e.update();
 });
 
-detectCollisions();
+dificultad();
 
 requestAnimationFrame(animate);
 
 }
 
-generateCircles(20);
+generarEstrellas(15);
 
 animate();
